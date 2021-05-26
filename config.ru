cc51b9faaa7b40ec7c316dea34972070258d70e8
Luciana Sugeta <lulusugeta@gmail.com>
2021-05-26 01:39:21 -0400

    commit tree 0f82d6ed071a042ec4069d9d5e09d2784fb8f351

--- config.ru
+++ config.ru
@@ -4,7 +4,12 @@ begin
   fi_check_migration
 
   use Rack::MethodOverride
+
+  use ArtistsController
+  use SongsController
+  use GenresController
   run ApplicationController
+  
 rescue ActiveRecord::PendingMigrationError => err
   STDERR.puts err
   exit 1
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,79 @@
 # Add seed data here. Seed your database with `rake db:seed`
+artists_list = {
+    "Justin Bieber" => {
+    },
+    "Adele" => {
+    },
+    "Drake" => {
+    },
+    "Jenifer Lopez" => {
+    },
+    "Backstreet Boys" => {
+    },
+    "Beyonce" => {
+    },
+    "Khalid" => {
+    },
+    "Dua Lipa" => {
+    },
+    "Harry Styles" => {
+    },
+    "Lil Nas X" => {
+    }
+}
 
+artists_list.each do |name, artist_hash|
+    p = Artist.new
+    p.name = name
+    p.save
+end
+
+songs_list = {
+    "Peaches" => {
+    },
+    "Hello" => {
+    },
+    "God's Plan" => {
+    },
+    "If You Had My Love" => {
+    },
+    "Everybody" => {
+    },
+    "Crazy In Love" => {
+    },
+    "Location" => {
+    },
+    "Levitating" => {
+    },
+    "Watermelon Sugar" => {
+    },
+    "Old Town Road" => {
+    }
+}
+
+songs_list.each do |name, song_hash|
+    p = Song.new
+    p.name = name
+    p.save
+end
+
+genres_list = {
+    "Pop" => {
+    },
+    "Country" => {
+    },
+    "Hip Hop" => {
+    },
+    "Soul" => {
+    },
+    "Rock" => {
+    },
+    "Rap" => {
+    }
+}
+
+genres_list.each do |name, genre_hash|
+    p = Genre.new
+    p.name = name
+    p.save
+end
